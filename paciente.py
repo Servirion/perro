6f6928e96376f60b1acb343eb4d4df1b47947e41
Servirion <sebastian.sanmartin36@inacapmail.cl>
2026-09-21 16:06:44 +0000

    avance min y .gitignore

--- paciente.py
+++ paciente.py
@@ -1,5 +1,6 @@
 class Paciente:
-    def _init_(self, rut:str, nombre:str, edad:int, prevision:str):
+    PREVICIONES:set[str] = {"Fonasa","Isapre","Particular"}
+    def __init__(self, rut:str, nombre:str, edad:int, prevision:str):
         self.rut = rut
         self.nombre = nombre
         self.edad = edad
@@ -20,3 +21,25 @@ class Paciente:
     @nombre.setter
     def nombre(self, nombre:str)->None:
         self._nombre = nombre
+
+    @property
+    def edad(self)->int:
+        return self._edad
+
+    edad.setter
+    def edad(self, edad:int)->None:
+        self._edad = edad
+
+    @property
+    def prevision(self)->str:
+            return self._prevision
+
+    prevision.setter
+    def prevision(self, prevision:str)->None:
+        self._prevision = prevision
+
+    def __str__(self)->str:
+        return f"Informacion del paciente:\nRUT: {self.rut}\nNombre: {self.nombre}\nEdad: {self.edad}\Prevision: {self.prevision}"
+
+    def __repr__(self)->str:
+        return f"Paciente(rut='{self.rut}', nombre='{self.nombre}', edad='{self.edad}', prevision='{self.prevision}')"
